dfc40affb6978a2d2403796c96865da203b18e65
lauren defrancesco <lauren209@live.com>
2018-03-06 14:09:51 -0500

    put the method names in

---
+++ include/superhero.h
@@ -0,0 +1,8 @@
+
+#include <string>
+#include <iostream>
+
+
+boolean insert(const Superhero & s);
+
+Superhero & get(const std::string name);
---
+++ src/superhero.cpp
@@ -0,0 +1,5 @@
+
+#include "../include/superhero.h"
+#include <string>
+#include <iostream>
+
