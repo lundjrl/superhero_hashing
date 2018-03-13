f9dd63753174dda579d4856813666c733f32a201
James Lund <lundj@eos11.cis.gvsu.edu>
2018-03-13 15:46:49 -0400

    ahhh

--- include/superhero.h
+++ include/superhero.h
@@ -9,22 +9,21 @@ class hash{
 
 	private:
 		int tableSize;
-		
+
 		//Definition of a superhero
-		struct Superhero{
-			int page_id;
-			std::string name;
-			std::string urlslug;
-			std::string id;
-			std::string alignment;
-			char eye_color;
-			char hair_color;
-			char sex;
-			std::string gsm;
-			bool alive;
-			int appearances;
-			std::string first_appearance;
-			int year;
+		int page_id;
+		std::string name;
+		std::string urlslug;
+		std::string id;
+		std::string alignment;
+		char eye_color;
+		char hair_color;
+		char sex;
+		std::string gsm;
+		bool alive;
+		int appearances;
+		std::string first_appearance;
+		int year;
 };
 
 #endif
--- src/superhero.cpp
+++ src/superhero.cpp
@@ -4,16 +4,18 @@
 #include <iostream>
 #include <cstdlib>
 
+
 /**
- * "A hash routine for string objects"
- *  ^^ Hash function from book, pg 195 Fig 5.4
- */
+ *  * "A hash routine for string objects"
+ *   *  ^^ Hash function from book, pg 195 Fig 5.4
+ *    */
 unsigned int hash(const string & key, int tableSize){
-	
-	unsigned int hashVal = 0;
-	
-	for(char ch : key){
-		hashVal = 37 * hashVal + ch;
+
+        unsigned int hashVal = 0;
+
+        for(char ch : key){
+                hashVal = 37 * hashVal + ch;
 }
-	return hashVal % tableSize;
+        return hashVal % tableSize;
 }
+~    
