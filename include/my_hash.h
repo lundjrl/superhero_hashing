08450527f9e66102949c4563108f79c6f70ea8f7
James Lund <lundj@eos11.cis.gvsu.edu>
2018-03-14 16:29:29 -0400

    Hopefully added read from file function, still need to test and implement

--- include/my_hash.h
+++ include/my_hash.h
@@ -1,14 +1,51 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 
 #ifndef MYHASH_H
 #define MYHASH_H
 
+class hash{
 
-bool insert(const Superhero & s);
+	std::string fileName;
+	std::string delimeter;
 
-Superhero & get(const std::string name);
+public:
+	hash(std::string filename, std::string delm = ",");
+		fileName(filename), delimeter(delm){}
 
+	//Function to grab data from a CSV file
+	std::vector<std::vector<std::string> > getData();
+
+	bool insert(const Superhero & s);
+
+	Superhero & get(const std::string name);
+
+};
+
+/**
+ *Parses through csv file line by line and returns to vector
+ */
+std::vector<std::vector<std::string> > hash::getData(){
+	std::ifstream file(fileName);
+	
+	std::vector<std::vector<std::string> > dataList;
+
+	std::string line = "";
+
+	//Iterate each line and split superheros using delimeter
+	while(getline(file, line)){
+		std::vector<std::string> vec;
+		boost::algorithm::split(vec, line, boost::is_any_of(delimeter));
+		dataList.push_back(vec);
+}
+	file.close();
+	
+	return dataList;
+}
 
 #endif  //MYHASH_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
-
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
 #include <string>
+#include <algorithm>
 #include <iostream>
 #include <cstdlib>
 #include "my_hash.h"
 
 int main(int argc, char** argv){
 
-	int index;
-	//hash hashObj;
-	//index = hashObj.Hash("James");
+	hash reader("NAME_OF_FILE.csv");
+
+	//Get data from CSV file
+	std::vector<std::string> > dataList = reader.getData();
 	
-	//std::cout << "index = " << index << std::endl;
+	//Print content if needed
+	for(std::vector<std::string> vec : dataList){
+		for(std::string data : vec){
+			std::cout<<data << " , ";
+}
+		std::cout<<std::endl;
+}
+	return 0;
 }
