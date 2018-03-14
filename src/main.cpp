fa57cb80710b9fd3b6dea51d2193ced76a2e8969
James Lund <lundj@eos11.cis.gvsu.edu>
2018-03-14 18:26:31 -0400

    Fixed placement for csv reader

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,46 @@
 #include <cstdlib>
 #include "my_hash.h"
 
+class hash{
+
+        std::string fileName;
+        std::string delimeter;
+
+public:
+        hash(std::string filename, std::string delm = ",");
+                fileName(filename), delimeter(delm){}
+
+        //Function to grab data from a CSV file
+                std::vector<std::vector<std::string> > getData();
+        
+                bool insert(const Superhero & s);
+        
+                Superhero & get(const std::string name);
+        
+};
+
+/**
+ *  *Parses through csv file line by line and returns to vector
+ *
+ */
+std::vector<std::vector<std::string> > hash::getData(){
+        std::ifstream file(fileName);
+
+        std::vector<std::vector<std::string> > dataList;
+
+        std::string line = "";
+
+        //Iterate each line and split superheros using delimeter
+        while(getline(file, line)){
+        	std::vector<std::string> vec;
+                boost::algorithm::split(vec, line, boost::is_any_of(delimeter));                dataList.push_back(vec);
+}
+        file.close();
+        
+                                                                                return dataList;
+                                                                                }
+        
+
 int main(int argc, char** argv){
 
 	hash reader("NAME_OF_FILE.csv");
