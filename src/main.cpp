c23f053a37ad99fd7628690e361ddb59900900f2
James Lund <lundj@eos11.cis.gvsu.edu>
2018-03-16 12:00:14 -0400

    Added onto file reader

--- src/main.cpp
+++ src/main.cpp
@@ -8,49 +8,38 @@
 #include <cstdlib>
 #include "my_hash.h"
 
-class hash{
+//class hash{
 
-        std::string fileName;
-        std::string delimeter;
+  //      std::string fileName;
+  //      std::string delimeter;
 
-public:
-        hash(std::string filename, std::string delm = ",");
-                fileName(filename), delimeter(delm){}
+//public:
+       // hash(std::string filename, std::string delm = ",");
+      //          fileName(filename), delimeter(delm){}
 
         //Function to grab data from a CSV file
-                std::vector<std::vector<std::string> > getData();
+    //            std::vector<std::vector<std::string> > getData();
         
-                bool insert(const Superhero & s);
+  //              bool insert(const Superhero & s);
         
-                Superhero & get(const std::string name);
+//                Superhero & get(const std::string name);
         
-};
+//};
 
 /**
  *  *Parses through csv file line by line and returns to vector
  *
  */
-std::vector<std::vector<std::string> > hash::getData(){
-        std::ifstream file(fileName);
+std::vector<std::vector<std::string>> parseCSV(string inputFileName);
+        std::ifstream inputFile(inputFileName);
 
         std::vector<std::vector<std::string> > dataList;
 
-        std::string line = "";
-
-        //Iterate each line and split superheros using delimeter
-        while(getline(file, line)){
-        	std::vector<std::string> vec;
-                boost::algorithm::split(vec, line, boost::is_any_of(delimeter));                dataList.push_back(vec);
-}
-        file.close();
-        
-                                                                                return dataList;
-                                                                                }
-        
+        std::string line = "";     
 
 int main(int argc, char** argv){
 
-	hash reader("NAME_OF_FILE.csv");
+	hash reader("marvel-wikia-data.csv");
 
 	//Get data from CSV file
 	std::vector<std::string> > dataList = reader.getData();
